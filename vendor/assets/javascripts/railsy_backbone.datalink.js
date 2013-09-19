b12d62d481c7e6fcdd50de552f37ddf5764a1883
Weston Platter <westonplatter@gmail.com>
2013-09-19 00:12:15 -0600

    bring in "dataLink" functionality enabling 100% form use

--- vendor/assets/javascripts/railsy_backbone.datalink.js
+++ vendor/assets/javascripts/railsy_backbone.datalink.js
@@ -1,28 +1,43 @@
 // __NOTE:__ only overriding Backbone when `railsy_backbone (start) ... (end)` 
 // is explicitly called out.
 // 
-// Before addign this to the repo, I'd like to enable a configuration value to 
-// turn this feature on and off
-// 
-// 
-// (function($) {
-//   return $.extend($.fn, {
-//     backboneLink: function(model) {
-//       return $(this).find(":input").each(function() {
-//         var el, name;
-//         el = $(this);
-//         name = el.attr("name");
-//         model.bind("change:" + name, function() {
-//           return el.val(model.get(name));
-//         });
-//         return $(this).bind("change", function() {
-//           var attrs;
-//           el = $(this);
-//           attrs = {};
-//           attrs[el.attr("name")] = el.val();
-//           return model.set(attrs);
-//         });
-//       });
-//     }
-//   });
-// })(jQuery);
+// TODO
+// Enable a configuration value to turn this feature on/off
+
+(function($) {
+  
+  // `$.extend` enables us to chain functions onto jQuery selected DOM elements.
+  // For example, 
+  // 
+  //     this.$("form").backboneLink(@model)
+  // 
+  return $.extend($.fn, {
+    
+    // name function `backboneLine`
+    backboneLink: function(model) {
+      
+      // only target HTML input elements
+      return $(this).find(":input").each(function() {
+        var el, name;
+        el = $(this);
+        
+        // select the HTML input `name` attribute
+        name = el.attr("name");
+        
+        // TODO describe what this does
+        model.bind("change:" + name, function() {
+          return el.val(model.get(name));
+        });
+        
+        // re-set model specifical attribute when changed
+        return $(this).bind("change", function() {
+          var attrs;
+          el = $(this);
+          attrs = {};
+          attrs[el.attr("name")] = el.val();
+          return model.set(attrs);
+        });
+      });
+    }
+  });
+})(jQuery);
